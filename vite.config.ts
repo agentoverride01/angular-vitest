b0621f75d9d036d56f810e02d8e852579514e754
agentoverride01 <a27998599@gmail.com>
2024-04-06 22:46:26 +0800

    add comment to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 
 import angular from '@analogjs/vite-plugin-angular';
@@ -11,6 +12,9 @@ export default defineConfig(({ mode }) => ({
     include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     environment: 'happy-dom',
+    /// workaround solution to make
+    /// rxjs@6.x works
+    /// https://github.com/analogjs/analog/issues/1009
     server: {
       deps: {
         inline: [/fesm2022/]
